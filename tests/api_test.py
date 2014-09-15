418368136fe46d6bfd8dbfda618098addf974e0f
Gigi <gigi.sayfan@aclimalabs.com>
2014-09-15 01:50:23 -0700

    better organization of test infrastructure. split get_activities tests. added get_activity test.

--- tests/api_test.py
+++ tests/api_test.py
@@ -2,56 +2,63 @@ import json
 from unittest import TestCase
 
 from api.api import create_app
-from api.models import metadata, Activity
+from api.models import metadata, Activity, Score
 from tests.test_util import create_mem_db
 
 
 class DataAPITest(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        # 1. Create in-memory test metadata DB
-        cls.app = create_app()
-        cls.session = create_mem_db(metadata, cls.app.db)
-        cls.test_app = cls.app.test_client()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.app.db.restore_engine()
-        cls.test_app = None
-
     def setUp(self):
         """
         """
-        # # Replace get_session() to make sure our test session is used.
-        # # This is necessary because without it the metadata API gets
-        # # a different memory session and the test can verify it.
-        # self.original_get_session = sa_util.get_session
-        #
-        # sa_util.get_session = lambda x: self.session
-        #
-        # # Disable closing of the memory session to avoid wiping the database
-        # self.session.close = lambda: None
+        # 1. Create in-memory test metadata DB
+        self.app = create_app()
+        self.session = create_mem_db(metadata, self.app.db)
+        self.test_app = self.app.test_client()
+
+        # 2. Add a few activities
+        activities = []
+        for i in range(3):
+            a = Activity()
+            a.name = 'activity_{}'.format(i)
+            activities.append(a)
+            self.session.add(a)
+
+        # 3. Add a few scores
+        for i in range(3):
+            for j in range(2):
+                s = Score()
+                s.activity = activities[i]
+                s.rx = j == 0
+                s.reps = j
+
+        self.session.commit()
 
     def tearDown(self):
         """
         """
-        # sa_util.get_session = self.original_get_session
+        self.app.db.restore_engine()
+        self.test_app = None
 
-    def test_get_activities(self):
+    def test_get_activities_empty(self):
+        # Start with a fresh memory db
+        self.session = create_mem_db(metadata, self.app.db)
+
         url = '/api/v1.0/activities'
         response = self.test_app.get(url)
         result = json.loads(response.data)['result']
         self.assertEqual(0, len(result))
 
-        # Add a few activities
-        for i in range(3):
-            a = Activity()
-            a.name = 'activity_{}'.format(i)
-            self.session.add(a)
-        self.session.commit()
+    def test_get_activities(self):
+        url = '/api/v1.0/activities'
 
         response = self.test_app.get(url)
         result = json.loads(response.data)['result']
         self.assertEqual(3, len(result))
+
+    def test_get_activity(self):
+        url = '/api/v1.0/activity/1'
+        response = self.test_app.get(url)
+        result = json.loads(response.data)['result']
+        self.assertEqual('activity_0', result['name'])
 
 
