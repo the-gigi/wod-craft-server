c00834e36a4f2f11e2b5e35f480cab5bb4743ced
Gigi <gigi.sayfan@aclimalabs.com>
2016-05-22 11:37:55 -0700

    Added CORS support to allow cross-origin requests during debugging

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 #!flask/bin/python
 from wodcraft.api.api import create_app
 
-app = create_app()
+debug = True
 
-app.run(port=8888, debug=True)
+app = create_app(debug=debug)
+
+app.run(port=8888, debug=debug)
--- wodcraft/api/api.py
+++ wodcraft/api/api.py
@@ -1,4 +1,5 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.cors import CORS
 from wodcraft.api.routes import map_routes
 from wodcraft.api import resources
 
@@ -12,12 +13,15 @@ from flask import Flask
 from flask.ext.restful import Api
 
 
-def create_app():
+def create_app(debug=True):
     app = Flask(__name__)
     app.config.from_object('wodcraft.api.config')
     resources.db = app.db = SQLAlchemy(app)
     api = Api(app)
     map_routes(api)
+
+    if debug:
+        CORS(app)
 
     return app
 
